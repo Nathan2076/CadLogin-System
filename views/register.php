11585fead0cf29be21e25a29fdf2fbe48b91c0b5
Nathan2076 <68337317+Nathan2076@users.noreply.github.com>
2024-10-30 00:40:55 -0300

    Style pages

--- views/register.php
+++ views/register.php
@@ -5,7 +5,7 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Cadastre-se</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
+    <style><?php include "style.css" ?></style>
 </head>
 <body>
     <main>
